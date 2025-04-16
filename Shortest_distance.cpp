574093f7ca0b9df1e72b6669577a85b38d590ae0
Maryam Zabarjad Shiraz <maria.zabarjad@gmail.com>
2025-04-16 16:12:19 -0700

    Update Shortest_distance.cpp

--- Shortest_distance.cpp
+++ Shortest_distance.cpp
@@ -9,36 +9,59 @@ int BFS(vector<vector<int>>& grid){
 
   int m = grid.size(); 
   int n = grid[0].size(); 
+  vector<vector<int>> count(m, vector<int> (n,0)); 
+  vector<vector<int>> dist(m,vector<int> (n,0)); 
+  vector<int> direction={-1,0,1,0,-1}; 
+   
+  int totalBuilding=0; 
 
-  vector<vector<int>> distance(m, vector<int> (n); 
-  vector<vector<int>> count(m, vector<int>(n)); 
-  vector<int> direction = {-1,0,1,0,-1}; 
-  vector<vector<bool>> visited(m, vector<bool> (n, false)); 
-  int minDistance = INT_MAX; 
-  queue<pair<int,int>> q; 
-  int totalbuildings = 0; 
   for(int i=0; i<m; ++i){
-    for(int j=0; j<n ; ++j){
+    for(int j=0; j<n;++j){
       if(grid[i][j]==1){
-        int depth = 0; 
+        queue<pair<int,int>> q;
+        vector<vector<bool>> visited(m, vector<bool>(n,false)); 
+        int level =0; 
+        totalBuilding++; 
         q.push({i,j}); 
-        totalbuildings++; 
-        for(int k=0; k< q.size(); ++k){
-          auto [x,y] = q.front(); 
-          q.pop(); 
-          for(int t=0; t<4; ++t){
-            int dx= x+direction[k]; 
-            int dy = y+direction[k+1]; 
-            if(dx>=0 && dx<m && dy>=0 && dy<n && grid[dx][dy]==0 && !visited[dx][dy]){
-              visited[dx][dy] = true; 
-              q.push({dx,dy}); 
-              
+        while(!q.empty()){
+          level++; 
+          int size= q.size();
+          for(int p=0; p<size; ++p){
+            auto [x,y] = q.front(); 
+            q.pop(); 
+            for(int k =0; k<4; ++k){
+              int dx = x+direction[k]; 
+              int dy = y+direction[k+1]; 
+              if(dx>=0 && dx<m && dy>=0 && dy<n && grid[dx][dy]==0 && !visited[dx][dy]){
+                visited[dx][dy] = true; 
+                q.push({dx,dy}); 
+                dist[dx][dy]+=level; 
+                count[dx][dy]++; 
+                
+              }
             }
           }
         }
-        
       }
     }
   }
+  int minDistance = INT_MAX; 
+  for(int i=0; i<m; ++i){
+    for(int j=0; j<n; ++j){
+      if(totalBuilding == count[i][j] && grid[i][j]==0){
+        minDistance = min(minDistance, dist[i][j]); 
+      }
+    }
+  }
+  return (minDistance == INT_MAX) ? -1 : minDistance; 
   
+}
+int main() {
+    vector<vector<int>> grid = {
+        {1, 0, 2, 0, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+    cout << BFS(grid) << endl;  // Output: 7
+    return 0;
 }
